fb417d0bc5ae48c4bf7de743c43f0f64a24768eb
anna1 <anna19791@hotmail.com>
2022-07-21 20:17:28 +0200

    Added utils

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import json
 
 
-def load_candidates_from_json() -> list[dict]:
-    with open('candidates.json', 'r', encoding='utf-8') as file:
+def load_candidates_from_json(path:str) -> list[dict]:
+    with open(path, 'r', encoding='utf-8') as file:
         return json.load(file)
 #– возвращает список всех кандидатов
 
 
 def get_candidate(candidate_id: int) -> dict:
-    for candidate in load_candidates_from_json():
+    for candidate in load_candidates_from_json("C:\\Users\\anna1\\skypro_lessons\\hw_11\\candidates.json"):
         if candidate['id'] == candidate_id:
             return candidate
 # – возвращает одного кандидата по его id
@@ -16,8 +16,8 @@ def get_candidate(candidate_id: int) -> dict:
 
 def get_candidates_by_name(candidate_name: str) -> list[dict]:
     result = []
-    for candidate in load_candidates_from_json():
-        if candidate['name'] == candidate_name:
+    for candidate in load_candidates_from_json("C:\\Users\\anna1\\skypro_lessons\\hw_11\\candidates.json"):
+        if candidate['name'].split()[0].lower() == candidate_name.lower():
             result.append(candidate)
     return result
 # – возвращает кандидатов по имени
@@ -25,7 +25,7 @@ def get_candidates_by_name(candidate_name: str) -> list[dict]:
 
 def get_candidates_by_skill(skill_name: str) -> list[dict]:
     result = []
-    for candidate in load_candidates_from_json():
+    for candidate in load_candidates_from_json("C:\\Users\\anna1\\skypro_lessons\\hw_11\\candidates.json"):
         if skill_name in candidate['skills'].lower().split(', '):
             result.append(candidate)
     return result
